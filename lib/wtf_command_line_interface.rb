978a1ac4a68274e6d1921c68a4932812cd8e3a05
Kitty Davies <kittyfdavies@gmail.com>
2018-11-11 23:04:15 +0000

    building out WTFCLI Class

--- lib/wtf_command_line_interface.rb
+++ lib/wtf_command_line_interface.rb
@@ -1,1 +1,16 @@
-
+class WTFCommandLineInterface
+  
+  def run
+    make_articles
+    display_articles
+  end
+  
+  def make_articles
+    
+  end
+  
+  def display_articles
+    
+  end
+  
+end
